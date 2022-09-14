c04d6dab0ca3a91b1758bddbfe7aea00a33d314f
kashif delvi <kashif.delvi@maersk.com>
2022-09-14 13:17:36 +0530

    update

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV == 'production'? '/vueDeployment/': '/vueDeployment/'
+  publicPath: process.env.NODE_ENV == 'production'? '/vueDeployment/': '/'
 });
